eb9957b5e2c424e8e666bbf1b2f45ec21149baf2
Matan Gottlieb <matan.gottlieb@wisor.ai>
2026-07-02 15:21:29 +0300

    chore: sync in-repo cask to 0.1.6

--- packaging/Casks/lede.rb
+++ packaging/Casks/lede.rb
@@ -1,6 +1,6 @@
 cask "lede" do
-  version "0.1.5"
-  sha256 "508f7935e58af199946a5dad06c777550394d8027060e3fed84135ea62600c76"
+  version "0.1.6"
+  sha256 "e003d3d9987274296b55a4af803751c3e9f53287831743ef3e785e8c9e266d04"
 
   url "https://github.com/lede-md/lede/releases/download/v#{version}/Lede_#{version}_aarch64.dmg"
   name "Lede"
